f636e372dd93c064af02a03fcab19646f3f00f6e
fernando <fernando.cr100888@gmail.com>
2022-07-29 15:49:24 -0400

    sintaxis, debugging y comentarios

---
+++ debugging_and_comments.php
@@ -0,0 +1,21 @@
+<?php
+
+$personas = [ //esto es un array (arreglo o lista)
+    "Fernando" => 33,
+    "Shakira" => 45,
+    "Chewbacca" => 200
+];
+
+var_dump( $personas ); //muestra el array (arreglo o lista)
+
+echo "<br>"
+
+// print_r( $personas ); no funciona en el navegador
+
+#esto también sirve para comentar
+
+/*esto también es un comentario pero puede escribir en 
+varias líneas */
+
+?>
+
---
+++ sintaxis.php
@@ -0,0 +1,17 @@
+<?php
+
+echo "Sintaxis de PHP <br>";
+
+$nombre = "Fernando";
+
+$developer = "PHP developer";
+
+echo "hola, me llamo " . $nombre . " y soy " . $developer . "<br>";
+
+
+echo "mucho gusto" . "<br>";
+
+echo "el resultado de 10 x 5 es " . 10 * 5;
+
+?>
+

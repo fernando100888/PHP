988ae7d65ab55029d5df032d3b95e06d14c92c19
fernando <fernando.cr100888@gmail.com>
2022-07-30 22:23:24 -0400

    operadores lógicos

---
+++ operadores.php
@@ -0,0 +1,22 @@
+<?php
+
+//operadores lógicos
+
+$valor_1 = TRUE;
+$valor_2 = FALSE;
+
+//and 
+var_dump($valor_1 and $valor_2);
+var_dump($valor_1 && $valor_2);
+echo "<br>";
+
+
+//or
+var_dump($valor_1 and $valor_2);
+var_dump($valor_1 && $valor_2);
+echo "<br>";
+
+//not
+var_dump(!$valor_1);
+
+

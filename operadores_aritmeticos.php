09dcd59dee4f01295c20d956f745b0cb3eb47c9d
fernando <fernando.cr100888@gmail.com>
2022-07-30 22:40:21 -0400

    operadores aritméticos

---
+++ operadores_aritmeticos.php
@@ -0,0 +1,39 @@
+<?php
+
+//operadores aritméticos
+
+$resultado_1 =  5 + 3;
+echo $resultado_1;
+echo "<br>";
+
+
+$resultado_2 =  5 - 3;
+echo $resultado_2;
+echo "<br>";
+
+
+$resultado_3 =  5 * 3;
+echo $resultado_3;
+echo "<br>";
+
+
+$resultado_4 =  5 ** 3; //5 x 5 x 5
+echo $resultado_4;
+echo "<br>";
+
+
+echo (5 * 6) . " " . (80 / 4);
+echo "<br>";
+
+
+echo (5 / 2);
+echo "<br>";
+
+
+echo (5 % 2);
+echo "<br>";
+
+
+echo "7205 segundos convertidos a horas son: " . (7205 / 3600) . " horas";
+echo "<br>";
+echo "sobran: " . (7205 % 3600) . "segundos";

4b188a20e12a00d931db03782be270f696c6214d
fernando <fernando.cr100888@gmail.com>
2022-07-30 21:59:52 -0400

    tipos de datos

---
+++ tipos_de_datos.php
@@ -0,0 +1,27 @@
+<?php
+
+$numero_1 = 8;
+
+
+$numero_2 = 7;
+
+
+define ("NUMERO_PI", 3.14);
+
+echo $numero_1 + $numero_2;
+
+echo "<br>";
+
+echo NUMERO_PI;
+
+
+//TIPOS DE DE DATOS
+
+$numero = 0;
+
+var_dump($numero);
+
+echo "<br>";
+
+?>
+

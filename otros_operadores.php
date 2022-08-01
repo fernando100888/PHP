70018e7ce46bfec44a6231a75714c4efcecaa82a
fernando <fernando.cr100888@gmail.com>
2022-08-01 14:30:35 -0400

    otros operadores

---
+++ otros_operadores.php
@@ -0,0 +1,46 @@
+<?php
+
+// otros operadores
+
+//operador de asignación
+$a = 5 ;
+
+
+// operadores de incremento 
+$contador = 1 ;
+//vamos a incrementar 1
+//primer método
+$contador = $contador + 1 ;
+echo $contador ;
+
+//segundo método
+$contador += 1 ;
+echo $contador ;
+
+//tercer método
+$contador ++ ;
+echo $contador ;
+
+
+/*
+también existen los operadores:
+
+-=
+*=
+/=
+
+que van a restar, multiplicar y dividir la variable entre un número cualquiera designado
+*/ 
+
+
+// operador de concatenación (útil para los strings)
+$nombre = "fernando" ;
+$nombre .= " " . "está programando en php" ;
+
+
+
+
+
+
+
+
